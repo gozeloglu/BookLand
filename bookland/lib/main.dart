33c0b8529a3f43fb9138336505dacd4eea4563c6
Nur Tek <nurbuke.teker7@gmail.com>
2020-03-22 16:10:17 +0300

    Main_page , drawer page added . Login_page connection handeled.

--- bookland/lib/main.dart
+++ bookland/lib/main.dart
@@ -1,127 +1,379 @@
 import 'package:flutter/material.dart';
 import 'package:bookland/login.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'Login';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: LoginStatefulWidget(),
+
+      title: 'BookLand',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyStatelessWidget(),
+
+
     );
   }
 }
 
-class LoginStatefulWidget extends StatefulWidget {
-  LoginStatefulWidget({Key key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
   @override
-  _MyLoginPageState createState() => _MyLoginPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyLoginPageState extends State<LoginStatefulWidget> {
-  int _selectedIndex = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  static const TextStyle optionStyle = TextStyle(
-      fontSize: 30,
-      fontWeight: FontWeight.bold);
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Categories',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Explore',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Book Cart',
-      style: optionStyle,
-    )
-  ];
-
-  void _onItemTapped(int index) {
+  void _incrementCounter() {
     setState(() {
-      _selectedIndex = index;
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the LoginPage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("BookLand"),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.account_circle,
-              color: Colors.white,
+        title: Text(widget.title),
+      ),
 
-            ),
-            onPressed: () {
-              Navigator.push(
-                  context, new MaterialPageRoute(builder: (context) => new Login()),
-              );
-              // TODO Login page will be here
-            },
-          )
-        ],
-      ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+
+        child: Column(
+
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.cyan,
-        unselectedItemColor: Colors.yellowAccent,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            title: Text("Categories"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            title: Text("Explore"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            title: Text("Book Cart")
-          )
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-        onTap: _onItemTapped,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomAppBar(
+        child : Container(
+            height : 100.0,
+
+            child : Row(
+                children : <Widget>[
+                  Text("Rock Bottom -> "),
+                  Text("The Rock ->"),
+                  Icon(Icons.home),
+                  IconButton(
+                      icon : Icon(Icons.add_box),
+                      onPressed :() {
+                        print("Icon Vutton BottomAppBar Pressed !!");
+                      }
+                  ),
+                ]
+            )
+        ),
+        color : Colors.orangeAccent,
       ),
     );
   }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// This Widget is the main application widget.
+
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar'));
+
+final duplicateItems = List<String>.generate(10000, (i) => "Item $i");
+var items = List<String>();
+
+
+
+void openPage(BuildContext context) {
+  Navigator.push(context, MaterialPageRoute(
+    builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Search'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (value) {
+                  },
+                  //controller: editingController,
+                  decoration: InputDecoration(
+                      labelText: "Search",
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('book'), //https://blog.usejournal.com/flutter-search-in-listview-1ffa40956685
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  ));
+}
+
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+
+  MyStatelessWidget({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title:  const Text('BookLand', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)),
+        actions: <Widget>[
+          Container(
+              child :IconButton(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: Colors.white,
+
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context, new MaterialPageRoute(builder: (context) => new Login()),
+                  );
+                  // TODO Login page will be here
+                },
+              )
+              /***child : FlatButton(
+
+                  //child: new Text('OK', style: new TextStyle(color: Colors.white)),
+                  child: new Image.asset('assets/person.jpg'),
+                  padding: EdgeInsets.all(0.0),
+                  onPressed: () {},
+                  color: Colors.blue,
+                  )*/,width: 30.0, height: 30.0),
+          //Text('PROFILE', style: new TextStyle(color: Colors.white)),
+          IconButton(
+            icon: const Icon(Icons.search),
+            tooltip: 'Search Page',
+            onPressed: () {
+              openPage(context);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child : Column(
+            mainAxisAlignment: MainAxisAlignment.start ,
+            children: <Widget>[
+              /***
+               *
+                  - assets/last_r.jpg
+                  - assets/look_l.png
+                  - assets/best_seller.jpg
+                  - assets/toplist.png
+                  - assets/campaign.png
+
+               */
+
+              Container(
+
+                child : new FlatButton(
+                  //child: new Text('OK', style: new TextStyle(color: Colors.white)),
+                  child: new Image.asset('assets/toplist.png'),
+                  padding: EdgeInsets.all(1.0),
+                  onPressed: () {},
+                  color: Colors.black,
+
+                )
+                ,width: 400.0, height: 100.0,),
+              Text(" "),
+              Container(
+                child : new FlatButton(
+                  //child: new Text('OK', style: new TextStyle(color: Colors.white)),
+                  child: new Image.asset('assets/campaign.png'),
+                  onPressed: () {},
+                  color: Colors.pink,
+
+                )
+                ,width: 400.0, height: 100.0,),
+              Text(" "),
+              Container(
+                child : new FlatButton(
+                  //child: new Text('OK', style: new TextStyle(color: Colors.white)),
+                  child: new Image.asset('assets/last_r.jpg'),
+                  onPressed: () {},
+                  color: Colors.orange,
+
+                )
+                ,width: 400.0, height: 100.0,)
+              ,
+              Text(" "),
+              Container(
+                child : new FlatButton(
+                  //child: new Text('OK', style: new TextStyle(color: Colors.white)),
+                  child: new Image.asset('assets/look_l.png'),
+                  onPressed: () {},
+                  color: Colors.lightBlueAccent,
+
+                )
+                ,width: 400.0, height: 100.0,),
+
+              Container(
+                child : new FlatButton(
+                  //child: new Text('OK', style: new TextStyle(color: Colors.white)),
+                  child: new Image.asset('assets/best_seller.png'),
+                  onPressed: () {},
+                  color: Colors.red,
+
+                )
+                ,width: 400.0, height: 100.0,)]),
+
+
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+
+            new UserAccountsDrawerHeader(accountName: new  Text('Nurbüke TEKER'),
+              accountEmail: new Text('nurbuke.teker7@gmail.com'),
+              currentAccountPicture: new CircleAvatar(
+                backgroundColor: Colors.black,
+                child: new Text("NT"),
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/bookland__pp.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            new ListTile(
+              title: new Text("Account"),
+              trailing: new Icon(Icons.account_circle),
+              onTap: (){
+
+              },
+            ),
+//Section Line
+            new Divider(),
+            new ListTile(
+              title: new Text("Orders"),
+              trailing: new Icon(Icons.add_shopping_cart),
+              onTap: (){
+              },
+            ),
+//Section Line
+            new Divider(),
+
+            new ListTile(
+              title: new Text("Library"),
+              trailing: new Icon(Icons.library_books),
+              onTap: (){
+              },
+            ),
+
+            new Divider(),
+            new ListTile(
+              title: new Text("Campaigns"),
+              trailing: new Icon(Icons.notifications_active),
+              onTap: (){
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("Manuels"),
+              trailing: new Icon(Icons.help),
+              onTap: (){
+              },
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("Exit"),
+              trailing: new Icon(Icons.exit_to_app),
+              onTap: (){
+              },
+            ),
+            new Divider(),
+
+          ],
+        ),),
+      bottomNavigationBar: BottomAppBar(
+        child : Container(
+            height : 50.0,
+
+            child : Row(
+                children : <Widget>[
+                  Text("           "),
+                  IconButton(
+                      icon :  Icon(Icons.home),
+
+                      onPressed :() {
+                        print("Icon home Pressed !!");
+                      }
+                  ),
+                  Text("           "),
+                  IconButton(
+                      icon : Icon(Icons.category),
+
+                      onPressed :() {
+                        print("Icon category Pressed !!");
+                      }
+                  ),
+                  Text("           "),
+                  IconButton(
+                      icon : Icon(Icons.explore),
+
+                      onPressed :() {
+                        print("Icon explore Pressed !!");
+                      }
+                  ),
+                  Text("           "),
+                  IconButton(
+                      icon : Icon(Icons.shopping_basket),
+
+                      onPressed :() {
+                        print("Icon shopping_basket Pressed !!");
+                      }
+                  ),
+                ]
+
+            )
+
+        ),
+        color : Colors.blue,
+      ),
+    );
+  }
+
 }
