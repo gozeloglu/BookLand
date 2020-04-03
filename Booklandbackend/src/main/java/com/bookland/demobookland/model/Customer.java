ab6810d32799460da4445c4c88b455f8660b233f
b21627868 <b21627868@cs.hacettepe.edu.tr>
2020-04-04 00:35:58 +0300

    Password encrypt,email validation,password at least 8 char,and exception handling for sign-up form

--- Booklandbackend/src/main/java/com/bookland/demobookland/model/Customer.java
+++ Booklandbackend/src/main/java/com/bookland/demobookland/model/Customer.java
@@ -1,10 +1,14 @@
 package com.bookland.demobookland.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.text.DateFormat;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 @Data
@@ -17,22 +21,28 @@ public class Customer {
     private int customerId;
 
     @Column(name = "Name", nullable = false)
+    @NotBlank(message = "Name is Necessary")
     private String firstName;
 
     @Column(name = "Surname", nullable = false)
+    @NotBlank(message = "Surname is Necessary")
     private String surname;
 
     @Column(name = "Username", nullable = false)
+    @Email(message = "Email must be a valid email address")
+    @NotBlank(message = "Username is Necessary")
     private String email;
 
     @Column(name = "Password", nullable = false)
+    @NotBlank(message = "Password is Necessary")
+    @Size(min = 8,message = "Your password must be at least 8 characters")
     private String password;
 
+    @JsonFormat(pattern="yyyy-MM-dd")
     @Column(name = "DateOfBirth")
-    private DateFormat dateOfBirth;
+    private Date dateOfBirth;
 
     @Column(name = "PhoneNumber")
     private String phoneNumber;
-
 
 }
