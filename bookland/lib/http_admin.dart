80c9cff36fca83804fee9eacd9bdd62bd3636b06
Nur Tek <nurbuke.teker7@gmail.com>
2020-04-16 18:21:49 +0300

    Admin Update Book connection made with backend.

--- bookland/lib/http_admin.dart
+++ bookland/lib/http_admin.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:bookland/adminAddBook.dart';
+import 'package:bookland/adminUpdateBook.dart';
 import 'package:bookland/model_book.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -19,25 +20,25 @@ class HttpAdmin {
     http.Response response = await http.post('http://10.0.2.2:8080/addBook',
       headers: <String, String>{'Authorization': basicAuth,'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: jsonEncode(<String, String>{
+      body: jsonEncode(<String, dynamic>{
         'bookId': isbn,
         "bookName": book_name,
         "bookImage": book_img,
         "author": book_author,
-        "price": book_price,
         "description": book_description,
         "category" : book_category,
-        "subCategory" : book_sub_category
+        "subCategory" : book_sub_category,
+        "priceList": [
+          {
+            "price": book_price
+          },
+        ],
       }
       ),
     );
 
     print(response.statusCode);
     if (response.statusCode < 400) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-      // User user = User.fromJson(json.decode(response.body));
-      //print(user.FirstName);
       print(response.body);
       print("YEHUUUU");
       return  "PERFECT";
@@ -64,10 +65,42 @@ class HttpAdmin {
 
     print(response.statusCode);
     if (response.statusCode < 400) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-      // User user = User.fromJson(json.decode(response.body));
-      //print(user.FirstName);
+      print(response.body);
+      print("YEHUUUU");
+      return  "PERFECT";
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      print("Bloweddd");
+      throw Exception('Failed to load album');
+      return "SORRRY" ;
+    }
+  }
+  Future<String> adminUpdateBook(String isbn,String book_quantity,String book_price,String book_description) async {
+    var client = http.Client();
+    var url = "http://10.0.2.2:8080";
+    String username = 'Daryl';
+    String password = 'WalkingDead';
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$username:$password'));
+
+    http.Response response = await http.put('http://10.0.2.2:8080/updateBook/${isbn}',
+      headers: <String, String>{'Authorization': basicAuth,'Content-Type': 'application/json; charset=UTF-8',
+      },
+        body: jsonEncode(<String, dynamic>{
+        "priceList": [
+          {
+            "price": book_price
+          },
+          ],
+        "description": book_description,
+          "quantity" : book_quantity
+        }
+    ),
+    );
+
+    print(response.statusCode);
+    if (response.statusCode < 400) {
       print(response.body);
       print("YEHUUUU");
       return  "PERFECT";
