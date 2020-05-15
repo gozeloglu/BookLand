4b5f4465e18e1725e21f2fdc25ecaee1222d8bf0
b21627868 <b21627868@cs.hacettepe.edu.tr>
2020-05-15 17:03:13 +0300

    Customer manage and deactivate account feature added

--- Booklandbackend/src/main/java/com/bookland/demobookland/controller/AdminController.java
+++ Booklandbackend/src/main/java/com/bookland/demobookland/controller/AdminController.java
@@ -5,7 +5,11 @@ import com.bookland.demobookland.services.AdminServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AdminController {
@@ -18,7 +22,15 @@ public class AdminController {
         return adminServices.getAdmin();
     }
 
+    @GetMapping(value = "/manageCustomers/{pageNo}/{pageSize}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CustomerInfoProjection> getCustomers(@PathVariable Integer pageNo, @PathVariable Integer pageSize) {
+        return adminServices.manageCustomers(pageNo - 1, pageSize);
+    }
 
+    @PutMapping(value = "/deActivateAccount/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String applyDiscount(@PathVariable Integer customerId) {
+        return adminServices.deActivateAccount(customerId);
+    }
 }
 
 
--- Booklandbackend/src/main/java/com/bookland/demobookland/services/AdminServices.java
+++ Booklandbackend/src/main/java/com/bookland/demobookland/services/AdminServices.java
@@ -1,9 +1,16 @@
 package com.bookland.demobookland.services;
 
+import com.bookland.demobookland.model.Customer;
 import com.bookland.demobookland.model.projections.CustomerInfoProjection;
 import com.bookland.demobookland.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServices {
@@ -13,6 +20,31 @@ public class AdminServices {
 
     public CustomerInfoProjection getAdmin() {
         return customerRepository.findByIsAdminEquals(1);
+    }
+
+    public List<CustomerInfoProjection> manageCustomers(Integer pageNo, Integer pageSize) {
+        Pageable paging = PageRequest.of(pageNo, pageSize);
+
+        Page<CustomerInfoProjection> pagedResult = customerRepository.findAllProjectedBy(paging);
+        return pagedResult.toList();
+    }
+
+    public String deActivateAccount(Integer customerId) {
+        try {
+            Optional<Customer> customer = customerRepository.findById(customerId);
+            if(customer.isPresent()){
+                Customer existingCustomer = customer.get();
+                existingCustomer.setStatus(0);
+                customerRepository.save(existingCustomer);
+                return "Account Deactivated";
+            }else {
+                return "User Not Found";
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return "Some Problem Occured";
+        }
+
     }
 }
 
