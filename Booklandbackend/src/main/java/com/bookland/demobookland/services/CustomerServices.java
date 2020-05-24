f3e67014b461d882196545e331dc4aae86dd6f3e
b21627868 <b21627868@cs.hacettepe.edu.tr>
2020-05-24 15:36:35 +0300

    best seller,wish list added

--- Booklandbackend/src/main/java/com/bookland/demobookland/services/CustomerServices.java
+++ Booklandbackend/src/main/java/com/bookland/demobookland/services/CustomerServices.java
@@ -1,8 +1,10 @@
 package com.bookland.demobookland.services;
 
+import com.bookland.demobookland.model.Book;
 import com.bookland.demobookland.model.Comment;
 import com.bookland.demobookland.model.Customer;
 import com.bookland.demobookland.model.projections.LoginInterface;
+import com.bookland.demobookland.repository.BookRepository;
 import com.bookland.demobookland.repository.CommentRepository;
 import com.bookland.demobookland.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.LoginException;
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 
@@ -22,6 +25,9 @@ public class CustomerServices {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @Autowired
     private CommentRepository commentRepository;
@@ -67,37 +73,80 @@ public class CustomerServices {
         return commentRepository.save(comment);
     }
 
-    public Customer updateCustomer(Customer customer,Integer customerId) throws LoginException{
+    public Customer updateCustomer(Customer customer, Integer customerId) throws LoginException {
         try {
             Customer currentCustomer = customerRepository.findByCustomerId(customerId);
-            if(customer.getFirstName() != null){
+            if (customer.getFirstName() != null) {
                 currentCustomer.setFirstName(customer.getFirstName());
             }
-            if(customer.getSurname() != null){
+            if (customer.getSurname() != null) {
                 currentCustomer.setSurname(customer.getSurname());
             }
-            if(customer.getDateOfBirth() != null){
+            if (customer.getDateOfBirth() != null) {
                 System.out.println(customer.getDateOfBirth());
                 currentCustomer.setDateOfBirth(customer.getDateOfBirth());
             }
-            if(customer.getPhoneNumber() != null){
+            if (customer.getPhoneNumber() != null) {
                 currentCustomer.setPhoneNumber(customer.getPhoneNumber());
             }
-            if(customer.getEmail() != null){
+            if (customer.getEmail() != null) {
                 Customer existingCustomer = customerRepository.findByEmail(customer.getEmail());
-                if(existingCustomer == null){
+                if (existingCustomer == null) {
                     currentCustomer.setEmail(customer.getEmail());
-                }else {
+                } else {
                     throw new LoginException("Email already exist");
                 }
             }
-           return customerRepository.save(currentCustomer);
-        }catch (Exception e){
+            return customerRepository.save(currentCustomer);
+        } catch (Exception e) {
             return null;
         }
     }
 
-    /*public Comment comment(Comment comment) {
-        return commentRepository.save(comment);
-    }*/
+    @Transactional
+    public Integer addWishList(Integer customerId, Integer isbn) {
+        Customer customer = customerRepository.findByCustomerId(customerId);
+        boolean inWishList = false;
+
+        if (customer.getCustomerWishList().size() == 0) {
+            Optional<Book> book = bookRepository.findById(isbn);
+            customer.getCustomerWishList().add(book.get());
+            inWishList = true;
+        } else {
+            for (int i = 0; i < customer.getCustomerWishList().size(); i++) {
+                if (!customer.getCustomerWishList().get(i).getBookId().equals(isbn)) {
+                    Optional<Book> book = bookRepository.findById(isbn);
+                    if (book.isPresent()) {
+                        customer.getCustomerWishList().add(book.get());
+                        inWishList = true;
+                        break;
+                    }
+                }
+            }
+        }
+        customerRepository.save(customer);
+        if (inWishList) {
+            return 1;
+        } else return 0;
+    }
+
+    @Transactional
+    public Integer removeWishList(Integer customerId, Integer isbn) {
+        Customer customer = customerRepository.findByCustomerId(customerId);
+        boolean inWishList = true;
+        for (int i = 0; i < customer.getCustomerWishList().size(); i++) {
+            if (customer.getCustomerWishList().get(i).getBookId().equals(isbn)) {
+                Optional<Book> book = bookRepository.findById(isbn);
+                if (book.isPresent()) {
+                    customer.getCustomerWishList().remove(book.get());
+                    customerRepository.save(customer);
+                    inWishList = false;
+                    break;
+                }
+            }
+        }
+        if (!inWishList) /*wishlist den remove olursa 1 dönüyo*/
+            return 1;
+        else return 0;
+    }
 }
