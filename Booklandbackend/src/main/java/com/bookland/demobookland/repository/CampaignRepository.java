e419e7cae2fe5c5d3e2939dc0493648b3db299b2
b21627868 <b21627868@cs.hacettepe.edu.tr>
2020-05-25 13:38:41 +0300

    code fixed

--- Booklandbackend/src/main/java/com/bookland/demobookland/repository/CampaignRepository.java
+++ Booklandbackend/src/main/java/com/bookland/demobookland/repository/CampaignRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CampaignRepository extends PagingAndSortingRepository<Campaign,Integer> {
+public interface CampaignRepository extends PagingAndSortingRepository<Campaign, Integer> {
 
     Campaign findByCouponCode(String couponCode);
 
