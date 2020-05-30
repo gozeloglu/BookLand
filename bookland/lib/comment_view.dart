149c08d45d349e5f94015f28a2e4546d45af26f8
gozeloglu <gozeloglu@gmail.com>
2020-05-30 19:28:15 +0300

    no comment message is added

--- bookland/lib/comment_view.dart
+++ bookland/lib/comment_view.dart
@@ -2,10 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bookland/http_comment_vote.dart';
 
-
 class CommentView extends StatefulWidget {
-
   final String bookId;
+
   CommentView(this.bookId);
 
   @override
@@ -22,6 +21,7 @@ class _CommentViewState extends State<CommentView> {
         appBar: AppBar(
           centerTitle: true,
           title: Text("Comments"),
+          backgroundColor: Colors.blue,
         ),
         body: commentPage());
   }
@@ -42,19 +42,13 @@ class _CommentViewState extends State<CommentView> {
     );
   }
 
-  Widget commentList() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [comment()],
-    );
-  }
-
   Widget buttons() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [backButton(), nextButton()],
     );
   }
+
   Widget comment() {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
@@ -62,56 +56,57 @@ class _CommentViewState extends State<CommentView> {
         future: commentVote.getComments(widget.bookId, page),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            print("HAS DATA");
-            List<String> commentList = [];
-            List<String> commenterList = [];
+            if (snapshot.data.length == 0) {
+              noComment();
+            } else {
+              print("HAS DATA");
+              List<String> commentList = [];
+              List<String> commenterList = [];
 
-            print("SNAPSHOT");
-            print(snapshot.data);
-            for (int i = 0; i < snapshot.data.length; i++) {
-              commentList.add(snapshot.data[i]["commentText"]);
-              commenterList.add(snapshot.data[i]["commenterName"] +
-                  " " +
-                  snapshot.data[i]["commenterSurname"]);
+              print("SNAPSHOT");
+              print(snapshot.data);
+              for (int i = 0; i < snapshot.data.length; i++) {
+                commentList.add(snapshot.data[i]["commentText"]);
+                commenterList.add(snapshot.data[i]["commenterName"] +
+                    " " +
+                    snapshot.data[i]["commenterSurname"]);
+              }
+
+              return ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    // TODO Card can be changed
+                    return Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 7,
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(10)),
+                      margin: const EdgeInsets.all(10),
+                      child: ListTile(
+                          title: Text(commenterList[index]),
+                          subtitle: Text(commentList[index])),
+                    );
+                  });
             }
-
-            return ListView.builder(
-              physics: BouncingScrollPhysics(),
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  // TODO Card can be changed
-                  return Container(
-                    padding: EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 7,
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    margin: const EdgeInsets.all(10),
-                    child: ListTile(
-                        title: Text(commenterList[index]),
-                        subtitle: Text(commentList[index])),
-                  );
-                });
           } else if (!snapshot.hasData) {
             // TODO Not data
+            noComment();
             print("No comment!");
-            return Container(
-              alignment: Alignment.center,
-              height: 160.0,
-              child: CircularProgressIndicator(),
-            );
           }
-          return Container(
-            alignment: Alignment.center,
-            height: 160.0,
-            child: CircularProgressIndicator(),
+          return Center(
+            child: Text(
+              "There is no comment for this book!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30),
+            ),
           );
         },
       ),
@@ -119,7 +114,6 @@ class _CommentViewState extends State<CommentView> {
   }
 
   Widget backButton() {
-
     return RaisedButton(
       child: Text(
         "Back",
@@ -131,13 +125,13 @@ class _CommentViewState extends State<CommentView> {
       ),
       disabledColor: Colors.grey,
       disabledTextColor: Colors.white,
-      onPressed: page > 1 ? () {
-        setState(() {
-          page--;
-        });
-        // TODO Retrieve previous page data
-
-    } : null,
+      onPressed: page > 1
+          ? () {
+              setState(() {
+                page--;
+              });
+            }
+          : null,
     );
   }
 
@@ -157,8 +151,17 @@ class _CommentViewState extends State<CommentView> {
         setState(() {
           page += 1;
         });
-        // TODO Retrieve next page data
       },
+    );
+  }
+
+  Widget noComment() {
+    return Center(
+      child: Text(
+        "There is no comment for this book!",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 30),
+      ),
     );
   }
 }
